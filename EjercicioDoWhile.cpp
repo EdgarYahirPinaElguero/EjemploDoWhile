e2960735edb4d37c37e3c3195121e0fa37a42f88
EdgarYahirPinaElguero <89890681+EdgarYahirPinaElguero@users.noreply.github.com>
2022-07-30 19:27:02 -0500

    Create EjercicioDoWhile.cpp

---
+++ EjercicioDoWhile.cpp
@@ -0,0 +1,16 @@
+package EjemploDoWhile3;
+import java.util.*;
+
+public class Numero3 {
+	public static void main (String[] args) {
+		int i=1;
+		System.out.println("Numero del 1 al 10: ");
+		
+		do {
+			System.out.print( i + "_");
+			i++;
+		}
+		while(i<=10);
+	}
+
+}
